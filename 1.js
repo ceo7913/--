4f15a5abeaf4b95477e62e2989fe0bf460d34f0f
ceo7913 <ceo7913@naver.com>
2022-09-23 17:18:08 +0900

    20220923프로젝트

---
+++ 1.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".swiper-container", {
+  pagination: ".swiper-pagination",
+  direction: "vertical",
+  slidesPerView: 1,
+  paginationClickable: true,
+  spaceBetween: 30,
+  mousewheelControl: true,
+  parallax: true,
+  speed: 600,
+});
